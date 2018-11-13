da65e159b23a09efdd43291a7a7b1e0b3fd6729c
eternal12345 <34204787+eternal12345@users.noreply.github.com>
2018-11-13 21:30:19 +0800

    初始化平台及模型userprofile的定义
    初始化平台及模型userprofile的定义

---
+++ nuckaggle/kaggle/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KaggleConfig(AppConfig):
+    name = 'kaggle'
---
+++ nuckaggle/kaggle/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class UserProfile(models.Model):
+    sex_is = (
+        ('man', '男'),
+        ('woman', '女'),
+    )
+    user = models.OneToOneField(User)
+    college = models.CharField("学校", max_length=10)
+    student_id = models.CharField("学号", max_length=15)
+    phone = models.CharField("联系电话", max_length=12)
+    sex = models.CharField("性别", max_length=10, choices=sex_is)
+
+    def __str__(self):
+        return str(self.user)
+
+    class Meta:
+        verbose_name_plural = '个人信息'
+
+
+
+
