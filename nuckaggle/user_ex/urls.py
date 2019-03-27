1404c482e3de4be9b9bc4f3138fd100afa6fd0d1
wxk <3021667395@qq.com>
2019-03-27 20:16:38 +0800

    验证邮箱注册否&页面拦截&重置密码初探

--- nuckaggle/user_ex/urls.py
+++ nuckaggle/user_ex/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^identify',views.identify,name='identify'),
+    url(r'^reset/<str:active_code>',ResetView.as_view(),name='reset'),
 ]
--- nuckaggle/user_ex/views.py
+++ nuckaggle/user_ex/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import IdentifyForm
 from .utils import send_forget_email
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
@@ -13,8 +14,15 @@ def identify(request):
 		form = IdentifyForm(request.POST)
 		if form.is_valid():
 			email=request.POST.get('email','')
-			send_forget_email(email)
-			return render(request,'user_ex/send_successful.html')
+			us = User.objects.filter(email = email)
+			if us:
+				send_forget_email(email)
+				return render(request,'user_ex/send_successful.html')
+			else:
+				context["identify_from"] = identify_from
+				context["statu"] = 1
+				context["error"] = "此邮箱未注册" 
+				return render(request,'user_ex/identify.html',context)
 		else:
 			context["identify_from"] = identify_from
 			context["statu"] = 1
@@ -24,3 +32,5 @@ def identify(request):
 		context["identify_from"] = identify_from
 		context["statu"] = 0
 		return render(request,'user_ex/identify.html',context)
+
+def reset_password(request):
