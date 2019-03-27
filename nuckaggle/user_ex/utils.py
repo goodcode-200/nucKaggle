2c6588ca8fb8ae5c90ccc0858c6ae63e409b1c61
wxk <3021667395@qq.com>
2019-03-27 22:42:23 +0800

    为了部署，更改发送链接域名

--- nuckaggle/user_ex/utils.py
+++ nuckaggle/user_ex/utils.py
@@ -21,7 +21,7 @@ def send_forget_email(email):
 	email_record.save()
  
 	email_title = '中北大学数据挖掘比赛网站密码重置链接'
-	email_body = '(如非本人操作,请忽略)请点击下面的链接重置你的密码：http://localhost:8000/user_ex/reset/{0}/{1}'.format(email,code)
+	email_body = '(如非本人操作,请忽略)请点击下面的链接重置你的密码：http://datsec.cn:8000/user_ex/reset/{0}/{1}'.format(email,code)
 
 	send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
 	if send_status:
